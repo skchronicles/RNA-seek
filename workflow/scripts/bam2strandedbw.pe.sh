2ba560279bd325e52e552733ae48e348df521faf
skchronicles <kuhnsa2@vcu.edu>
2020-11-03 16:41:08 -0500

    Fixing GNU parallel citation acknowledgement and adding new positional ARG to fix issue when PATH contains "."

--- workflow/scripts/bam2strandedbw.pe.sh
+++ workflow/scripts/bam2strandedbw.pe.sh
@@ -1,20 +1,23 @@
-#author:Vishal Koparde
-#converts STAR aligned PE bam file to forward and reverse strand bigwigs
-module load samtools
-module load bedtools
-module load ucsc
-module load parallel
-bam=$1
+# @author: Vishal Koparde
+# @modified: Skyler Kuhn
+# @description: converts STAR aligned PE bam file to forward and reverse strand bigwigs
+
+bam="$1"
+outprefix="$2"
+
 samtools view -H $1|grep "^@SQ"|cut -f2,3|sed "s/SN://g"|sed "s/LN://g" > ${bam}.genome
+
 echo "samtools view -b -f 128 -F 16 $bam | bedtools genomecov -bg -split -ibam stdin -g ${bam}.genome > ${bam}.fwd1.bg && bedSort ${bam}.fwd1.bg ${bam}.fwd1.bg" > ${bam}.dobg
 echo "samtools view -b -f 80 $bam | bedtools genomecov -bg -split -ibam stdin -g ${bam}.genome > ${bam}.fwd2.bg && bedSort ${bam}.fwd2.bg ${bam}.fwd2.bg" >> ${bam}.dobg
 echo "samtools view -b -f 144 $bam | bedtools genomecov -bg -split -ibam stdin -g ${bam}.genome > ${bam}.rev1.bg && bedSort ${bam}.rev1.bg ${bam}.rev1.bg" >> ${bam}.dobg
 echo "samtools view -b -f 64 -F 16 $bam | bedtools genomecov -bg -split -ibam stdin -g ${bam}.genome > ${bam}.rev2.bg && bedSort ${bam}.rev2.bg ${bam}.rev2.bg" >> ${bam}.dobg
-parallel -j 4 < ${bam}.dobg
+parallel --will-cite -j 4 < ${bam}.dobg
+
 bedtools unionbedg -i ${bam}.fwd1.bg ${bam}.fwd2.bg|awk -F"\t" -v OFS="\t" '{sum=0;for (i=4;i<=NF;i+=1) {sum+=$i};print $1,$2,$3,sum}' - > ${bam}.fwd.bg
-bedtools unionbedg -i ${bam}.rev1.bg ${bam}.rev2.bg|awk -F"\t" -v OFS="\t" '{sum=0;for (i=4;i<=NF;i+=1) {sum+=$i};print $1,$2,$3,sum}' - > ${bam}.rev.bg 
-bedSort ${bam}.fwd.bg ${bam}.fwd.bg 
-bedGraphToBigWig ${bam}.fwd.bg ${bam}.genome ${bam%%.*}.fwd.bw
-bedSort ${bam}.rev.bg ${bam}.rev.bg 
-bedGraphToBigWig ${bam}.rev.bg ${bam}.genome ${bam%%.*}.rev.bw
+bedtools unionbedg -i ${bam}.rev1.bg ${bam}.rev2.bg|awk -F"\t" -v OFS="\t" '{sum=0;for (i=4;i<=NF;i+=1) {sum+=$i};print $1,$2,$3,sum}' - > ${bam}.rev.bg
+bedSort ${bam}.fwd.bg ${bam}.fwd.bg
+bedGraphToBigWig ${bam}.fwd.bg ${bam}.genome ${outprefix}.fwd.bw
+bedSort ${bam}.rev.bg ${bam}.rev.bg
+bedGraphToBigWig ${bam}.rev.bg ${bam}.genome ${outprefix}.rev.bw
+
 rm -f ${bam}.fwd*.bg ${bam}.rev*.bg ${bam}.dob? ${bam}.genome
--- workflow/scripts/bam2strandedbw.se.sh
+++ workflow/scripts/bam2strandedbw.se.sh
@@ -1,15 +1,17 @@
-#author:Vishal Koparde
-#converts STAR aligned SE bam file to forward and reverse strand bigwigs
-module load samtools
-module load bedtools
-module load ucsc
-module load parallel
-bam=$1
+# @author: Vishal Koparde
+# @modified: Skyler Kuhn (add output prefix) due to problem with '.' in output PATH
+# @description: converts STAR aligned SE bam file to forward and reverse strand bigwigs
+
+bam="$1"
+outprefix="$2"
 samtools view -H $1|grep "^@SQ"|cut -f2,3|sed "s/SN://g"|sed "s/LN://g" > ${bam}.genome
+
 echo "samtools view -b -f 16 $bam | bedtools genomecov -bg -split -ibam stdin -g ${bam}.genome > ${bam}.fwd.bg && bedSort ${bam}.fwd.bg ${bam}.fwd.bg" > ${bam}.dobg
 echo "samtools view -b -F 16 $bam | bedtools genomecov -bg -split -ibam stdin -g ${bam}.genome > ${bam}.rev.bg && bedSort ${bam}.rev.bg ${bam}.rev.bg" >> ${bam}.dobg
-parallel -j 2 < ${bam}.dobg
-echo "bedGraphToBigWig ${bam}.fwd.bg ${bam}.genome ${bam%%.*}.fwd.bw" > ${bam}.dobw
-echo "bedGraphToBigWig ${bam}.rev.bg ${bam}.genome ${bam%%.*}.rev.bw" >> ${bam}.dobw
-parallel -j 2 < ${bam}.dobw
+parallel --will-cite -j 2 < ${bam}.dobg
+
+echo "bedGraphToBigWig ${bam}.fwd.bg ${bam}.genome ${outprefix}.fwd.bw" > ${bam}.dobw
+echo "bedGraphToBigWig ${bam}.rev.bg ${bam}.genome ${outprefix}.rev.bw" >> ${bam}.dobw
+parallel --will-cite -j 2 < ${bam}.dobw
+
 rm -f ${bam}.fwd*.bg ${bam}.rev*.bg ${bam}.do* ${bam}.genome
