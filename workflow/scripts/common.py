b68bd63f15aa03ed097f484379c11113a893ce9e
skchronicles <kuhnsa2@vcu.edu>
2020-11-17 18:29:50 -0500

    Adding common function to determine target files for optional steps

--- workflow/scripts/common.py
+++ workflow/scripts/common.py
@@ -1,1 +1,12 @@
 # Any Python script in the scripts folder will be able to import from this module.
+
+# Determine targets for optional rules
+def provided(samplelist, condition):
+    '''
+    Determines if optional rules should run. If an empty list is provided to rule all,
+    snakemake will not try to generate that set of target files. If a given condition
+    is not met (i.e. False) then it will not try to run that rule.
+    '''
+    if not condition:
+        samplelist = []
+    return samplelist
